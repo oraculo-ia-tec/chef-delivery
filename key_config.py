8e5c90ddc5b8c1f4b21361d22a706f21a20817aa
Oraculos IA <oraculosia@gmail.com>
2025-04-09 11:57:03 -0300

    Update key_config.py
    Atualização em key_config para configuração de variáveis de ambiente.

--- key_config.py
+++ key_config.py
@@ -21,10 +21,26 @@ if not WEBHOOK_URL:
 
 # Chave do Webhook do MAKE AGENDA
 WEBHOOK_AGENDA = config("WEBHOOK_AGENDA")
-if not WEBHOOK_URL:
+if not WEBHOOK_AGENDA:
     raise ValueError("A chave do Webhook não foi encontrada. Verifique a configuração.")
 
 # Chave do Webhook do MAKE TESTE
 WEBHOOK_TESTE = config("WEBHOOK_TESTE")
-if not WEBHOOK_AGENDA:
+if not WEBHOOK_TESTE:
     raise ValueError("A chave do Webhook não foi encontrada. Verifique a configuração.")
+
+
+# Chave do Webhook do MAKE TESTE
+WEBHOOK_CADASTRO = config("WEBHOOK_CADASTRO")
+if not WEBHOOK_CADASTRO:
+    raise ValueError("A chave do Webhook para cadastro não foi encontrada. Verifique a configuração.")
+
+# Chave do Webhook do MAKE TESTE
+DATABASE_URL = config("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("A url do banco de dados não foi encontrada. Verifique a configuração.")
+
+# Chave do Webhook do MAKE TESTE
+ASAAS_API_KEY = config("ASAAS_API_KEY")
+if not ASAAS_API_KEY:
+    raise ValueError("A chave não foi encontrada. Verifique a configuração.")
